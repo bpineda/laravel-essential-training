37c439459f03ceab7be7c83d67ffeb36bbb0f7ee
bpineda <_@bernardopineda.mx>
2017-05-07 22:15:17 -0500

    Added additional field for the clients table¡

--- code/database/migrations/2017_04_27_180506_create_clients_table.php
+++ code/database/migrations/2017_04_27_180506_create_clients_table.php
@@ -15,6 +15,7 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
             $table->string('name');
             $table->string('last_name');
             $table->string('address');
