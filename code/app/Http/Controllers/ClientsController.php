e3804f9040e8b27a825c0d988ca4a3de1cc37dc9
bpineda <_@bernardopineda.mx>
2017-05-07 22:23:35 -0500

    Added client index functionality to show all available clients on the DB

--- code/app/Http/Controllers/ClientsController.php
+++ code/app/Http/Controllers/ClientsController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client as Client;
+use Illuminate\Support\Facades\DB;
 
 class ClientsController extends Controller
 {
     function index()
     {
-        return view('clients/index', [] );
+        $client = new Client();
+        $data = [];
+        $data['clients'] = $client->all();
+        return view('clients/index', $data );
+    }
+
+    function newClient()
+    {
+        return view('clients/form', [] );
+    }
+
+    function show( $id )
+    {
+        return view('clients/form', [] );
     }
 }
