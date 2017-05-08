c0e7ea89dcc5f2667369cae329f709e575398920
bpineda <_@bernardopineda.mx>
2017-05-07 22:24:40 -0500

    Added client index functionality to show all available clients on the DB and to redirect to Book a Room page

--- code/resources/views/clients/index.blade.php
+++ code/resources/views/clients/index.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
       <div class="medium-12 large-12 columns">
         <h4>Clients</h4>
-        <div class="medium-2  columns"><a class="button hollow success" href="#">ADD NEW CLIENT</a></div>
+        <div class="medium-2  columns"><a class="button hollow success" href="{{ route('new_clients') }}">ADD NEW CLIENT</a></div>
 
         <table class="stack">
           <thead>
@@ -16,46 +16,17 @@
             </tr>
           </thead>
           <tbody>
-                          <tr>
-                <td>Mr. Roy Adams</td>
-                <td>radams1v@xinhuanet.com</td>
+            @foreach ($clients as $client)
+              <tr>
+                <td>{{ ucfirst( $client->title )  }}. {{ $client->name }} {{ $client->last_name }}</td>
+                <td>{{ $client->email }}</td>
                 <td>
-                  <a class="hollow button" href="#">EDIT</a>
-                  <a class="hollow button warning" href="#">BOOK A ROOM</a>
+                  <a class="hollow button" href="{{ route('show_clients', ['id' => $client->id]) }}">EDIT</a>
+                  <a class="hollow button warning" href="{{ route('reservations', ['user_id' =>$client->id]) }}">BOOK A ROOM</a>
                 </td>
               </tr>
-                          <tr>
-                <td>Mrs. Bonnie Clark</td>
-                <td>bclark6@bing.com</td>
-                <td>
-                  <a class="hollow button" href="#">EDIT</a>
-                  <a class="hollow button warning" href="#">BOOK A ROOM</a>
-                </td>
-              </tr>
-                          <tr>
-                <td>Ms. Carol Shaw</td>
-                <td>cshaw2e@marriott.com</td>
-                <td>
-                  <a class="hollow button" href="#">EDIT</a>
-                  <a class="hollow button warning" href="#">BOOK A ROOM</a>
-                </td>
-              </tr>
-                          <tr>
-                <td>Dr. Timothy Watson</td>
-                <td>twatson1k@shinystat.com</td>
-                <td>
-                  <a class="hollow button" href="#">EDIT</a>
-                  <a class="hollow button warning" href="#">BOOK A ROOM</a>
-                </td>
-              </tr>
-                          <tr>
-                <td>Ms. Judith Young</td>
-                <td>jyoung11@goodreads.com</td>
-                <td>
-                  <a class="hollow button" href="#">EDIT</a>
-                  <a class="hollow button warning" href="#">BOOK A ROOM</a>
-                </td>
-              </tr>
+              @endforeach
+
                       </tbody>
         </table>
       </div>
