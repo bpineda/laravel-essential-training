b25bae9c53a328df3f557bcc4384c2027ef1e50d
bpineda <_@bernardopineda.mx>
2017-05-07 22:27:03 -0500

    Added form data to be able to process dates; added a book room method to add a record to the reservations method

--- code/app/Http/Controllers/ReservationsController.php
+++ code/app/Http/Controllers/ReservationsController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Room as Room;
+use App\Client as Client;
+use App\Reservation as Reservation;
 
 class ReservationsController extends Controller
 {
-    function index()
+
+
+    function index(Request $request)
     {
-        return view('reservations/index', [] );
+
+        $dateFrom = $request->input("dateFrom");
+        $dateTo = $request->input("dateTo");
+        
+        $client = new Client();
+        $room = new Room();
+
+        $data = [];
+        $data['dateFrom'] = $dateFrom;
+        $data['dateTo'] = $dateTo;
+        $data['rooms'] = $room->getAvailableRooms($dateFrom, $dateTo);
+        $data['client'] = $client->find(1)->first();
+        return view('reservations/index', $data );
+    }
+
+    function bookRoom(  $client_id, 
+                        $room_id, 
+                        $date_in, 
+                        $date_out )
+    {
+        $reservation = new Reservation();
+        $reservation->bookRoom(   $client_id, 
+                                $room_id, 
+                                $date_in, 
+                                $date_out );
+        return redirect()->route('clients');
     }
 }
