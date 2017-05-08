ee41c6c0634095c12a8395ace76a3022ae9223a8
bpineda <_@bernardopineda.mx>
2017-05-07 22:19:51 -0500

    Added the belongs to relationship for both clients and room; Added method to book a room

--- code/app/Reservation.php
+++ code/app/Reservation.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     //
+    public function client()
+    {
+        return $this->belongsTo('App\Client');
+    }
+
+    public function room()
+    {
+        return $this->belongsTo('App\Room');
+    }
+
+    public function bookRoom(   $client_id, 
+                                $room_id, 
+                                $date_in, 
+                                $date_out )
+    {
+        $this->insert(
+            [
+                'date_in' => $date_in, 
+                'date_out' => $date_out,
+                'client_id' => $client_id,
+                'room_id' => $room_id
+            ]
+        );
+    }
+
 }
